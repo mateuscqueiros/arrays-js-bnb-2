afe404b36521fcfb8f62d9bf14986b4da1ed8508
Mateus Queirós <mateuscqueiros@gmail.com>
2024-09-05 12:00:53 -0300

    map e filter

--- array-2-exercises.js
+++ array-2-exercises.js
@@ -35,3 +35,59 @@ const users = [
     isActive: true,
   },
 ];
+
+/* EXERCÍCIOS de ARRAYS: https://www.notion.so/Arrays-II-f876bb1cab8b4863ad825bf42ad1f965
+
+/* MAP */
+
+// 1
+const userNames = users.map((user) => user.name);
+console.log('Map 1:', userNames);
+
+// 2
+const userEmailsUppercase = users.map((users) => users.email.toUpperCase());
+console.log('Map 2:', userEmailsUppercase);
+
+// 3
+const agePlusOne = users.map((user) => ({
+  ...user,
+  age: ++user.age,
+}));
+console.log('Map 3:', agePlusOne);
+
+// 4
+const activeStatusTransform = users.map((user) => ({
+  ...user,
+  isActive: user.isActive ? 'Sim' : 'Não',
+}));
+console.log('Map 4:', activeStatusTransform);
+
+// 5
+const idsNames = users.map((user) => ({
+  id: user.id,
+  name: user.name,
+}));
+console.log('Map 5:', idsNames);
+
+console.log('\n=========================================\n');
+/* FILTER */
+
+// 1
+const activeUsers = users.filter((user) => user.isActive);
+console.log('Filter 1:', activeUsers);
+
+// 2
+const olderThanThirdy = users.filter((user) => user.age > 30);
+console.log('Filter 2:', olderThanThirdy);
+
+// 3
+const emailExample = users.filter((user) => user.email.includes('example'));
+console.log('Filter 3:', emailExample);
+
+// 4
+const nameStartsWithJ = users.filter((user) => user.name.startsWith('J'));
+console.log('Filter 4:', nameStartsWithJ);
+
+// 5
+const idIsEven = users.filter((user) => user.id % 2 === 0);
+console.log('Filter 5:', idIsEven);
